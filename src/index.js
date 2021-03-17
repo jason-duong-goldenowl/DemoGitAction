27c1038b490975960d9037d82d9ed3a23b97426b
jason-duong-goldenowl <dirk.duong.goldenowl@gmail.com>
2021-03-17 15:26:53 +0700

    Edit File

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import {View, Text} from 'react-naitve'
 export default () => (
     <View>
-        <Text>Duong Quocs Khanhs</Text>
+        <Text>DEMO</Text>
     </View>
 )
