d86d51906487b19e72d3e2a154ef3758b7498158
jason-duong-goldenowl <dirk.duong.goldenowl@gmail.com>
2021-03-18 10:35:28 +0700

    new Project

---
+++ ios/Pods/Target Support Files/Pods-DemoGitAction-DemoGitActionTests/Pods-DemoGitAction-DemoGitActionTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoGitAction_DemoGitActionTests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoGitAction_DemoGitActionTests
+@end
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import {View, Text} from 'react-naitve'
-export default () => (
-    <View>
-        <Text>DEMO HAHA</Text>
-    </View>
-)
